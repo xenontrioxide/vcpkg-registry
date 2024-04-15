4751cc6823aea509c8f8b264b279f87d92fbd427
xenontrioxide <xenontrioxide@protonmail.com>
2024-04-14 22:37:49 -0700

    Fix SHA512 hash

--- ports/simple_error/portfile.cmake
+++ ports/simple_error/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO xenontrioxide/simple_error
     REF v1.0.0  # The tag or commit you want to download
-    SHA512 12ab34cd56...  # The SHA512 hash of the tar.gz file from GitHub release
+    SHA512 06D03FDC455B189E81480F40BADF1B7E89B6CDEA2E7162C1551AFF7BCD90BE1EAB0FDFC1737B665FB2A13B4CE531C35FF398ADF1E6D7CFFB9EE32141DAB696ED  # The SHA512 hash of the tar.gz file from GitHub release
     HEAD_REF main
 )
 
